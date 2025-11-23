1cc94e3c6596e0dc2672972d8e96b4e854a07bc4
Amen <krakenamen@gmail.com>
2025-11-23 18:26:49 +0100

    hoooks

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+function Abbout()
+{
+    return(
+        <><div>lllll</div>
+        <Link href="/">go home</Link></>
+    );
+}
+export default Abbout;
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,19 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header";
 import List from "@/components/List";
+import Abbout from "./about/page";
+import Link from "next/link";
 
 function Home() {
+  let p=true;
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <p>amen start react </p>
         <Header tx={"amegn"}
         t="fffff"/>
-        <List g="je suis un list"/>
+        {p &&< List g="je suis un list"/>}
+        <Link href="/about">do about</Link>
       </main>
     </div>
   );
